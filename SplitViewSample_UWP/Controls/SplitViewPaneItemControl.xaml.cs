1c26901af317b771760670272bb22420aa8835fd
logovaser <logovaser@gmail.com>
2015-11-28 04:59:35 +0200

    [FirstCommit]

---
+++ SplitViewSample_UWP/Controls/SplitViewPaneItemControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SplitViewSample_UWP.Controls
+{
+    public sealed partial class SplitViewPaneItemControl : UserControl
+    {
+        public SplitViewPaneItemControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Glyph
+        {
+            get { return this.GlyphTextBlock.Text; }
+            set { this.GlyphTextBlock.Text = value; }
+        }
+
+        public string Text
+        {
+            get { return this.NameTextBlock.Text; }
+            set { this.NameTextBlock.Text = value; }
+        }
+    }
+}
---
+++ SplitViewSample_UWP/View/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SplitViewSample_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ItemsList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = false;
+
+            ItemsList.SelectedItem = e.ClickedItem;
+
+            ChangeSidePanel((SlitViewListItem)ItemsList.SelectedIndex);
+        }
+
+        private void ChangeSidePanel(SlitViewListItem type)
+        {
+            switch (type)
+            {
+                case SlitViewListItem.Map:
+                    // Do something
+                    break;
+                case SlitViewListItem.Search:
+                    // Do something
+                    break;
+                case SlitViewListItem.Filters:
+                    // Do something
+                    break;
+                default:
+                    // Do something
+                    break;
+            }
+        }
+
+        private void ToggleSplitViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+    }
+
+    public enum SlitViewListItem
+    {
+        Map, Search, Filters
+    }
+}
